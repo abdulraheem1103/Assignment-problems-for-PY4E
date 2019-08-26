eece2255f19dc91bd5d92f5eede5fda008b22e3f
Abdul Raheem <32108855+abdulraheem1103@users.noreply.github.com>
2019-08-27 00:54:12 +0530

    Update Assignment 3.py

--- Assignment 3.py
+++ Assignment 3.py
@@ -6,6 +6,6 @@ r = float(rate)
 if hrs <= 40 :
     pay = h * rate
 else :
-    pay = r * 40 + (r * 1.5 * (h - 40))
+    pay = r * 40 + (r * 1.5 * (h - 40)) #this condition was given in the question
 
 print (pay)
