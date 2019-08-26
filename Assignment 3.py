00b7f9dd8ab8847983cecc4f8b67eb8d28c5b0ca
Abdul Raheem <32108855+abdulraheem1103@users.noreply.github.com>
2019-08-27 00:45:45 +0530

    Create Assignment 3.py

---
+++ Assignment 3.py
@@ -0,0 +1,11 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter Rate")
+r = float(rate)
+
+if hrs <= 40 :
+    pay = h * rate
+else :
+    pay = r * 40 + (r * 1.5 * (h - 40))
+
+print (pay)
